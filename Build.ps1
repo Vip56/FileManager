3306d1674b9169af108d5abe334924db72c21ecb
姚正发 <976691141@qq.com>
2016-12-14 11:32:37 +0800

    修改自动增加的版本号

--- Build.ps1
+++ Build.ps1
@@ -42,7 +42,7 @@ exec { & dotnet restore }
 Invoke-MSBuild
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+$revision = [convert]::ToInt32($revision, 10)
 
 #exec { & dotnet test .\test\Sino.FileManager.Tests -c Release }
 
